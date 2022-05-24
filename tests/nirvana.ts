0b12fbf49485293787d41c6d18918750e095c7b9
Clayons <admin@github.xyz>
2022-05-25 00:32:03 +0800

    test

--- tests/nirvana.ts
+++ tests/nirvana.ts
@@ -1,8 +1,18 @@
-import { Program, AnchorProvider, setProvider } from '@project-serum/anchor'
+import {
+  Program,
+  AnchorProvider,
+  setProvider,
+  utils,
+  BN,
+} from '@project-serum/anchor'
+import { PublicKey } from '@solana/web3.js'
+
 import { Nirvana } from '../target/types/nirvana'
 import idl from '../target/idl/nirvana.json'
 
 const programId = 'nirkXSE28jCQoK8SmKWqxXz3L9vbSRGKS24iYJj8Aeo'
+const nirv_center = 'NNNDj6CgMZbTiJmkvmK2TZwTThXgNWZrCk5bvL9f6Ee'
+const price_field_pda = 'BVVSjMb7evw5pWQaByRKpLjQerKVgVgCP8mh1PuALJF2'
 
 describe('nirvana', () => {
   // Configure the client to use the local cluster.
@@ -15,5 +25,23 @@ describe('nirvana', () => {
     provider
   )
 
-  it('price field', async () => {})
+  const seeds = [
+    Buffer.from(utils.bytes.utf8.encode('pf2')),
+    new PublicKey(nirv_center).toBuffer(),
+  ]
+
+  it('price field', async () => {
+    const [price_field] = await PublicKey.findProgramAddress(
+      seeds,
+      program.programId
+    )
+
+    const state = await program.account.priceFieldV2.fetch(price_field)
+
+    for (const i in state) {
+      if (i != 'nirvCenter' && i != 'bump') {
+        console.log(i, new BN(state[i]).toString())
+      }
+    }
+  })
 })
